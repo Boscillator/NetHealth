0da4e83d99a3546d9ad3afc311f90268ab528c12
Fred Buchanan <buchaf@ganxis3.nest.rpi.edu>
2020-11-06 14:17:16 -0500

    Added weighted edges to 03_find_components

--- src/03_find_components.py
+++ src/03_find_components.py
@@ -23,26 +23,13 @@ def parse_edge(edge):
 def make_grouper(period):
     def grouper(edge):
         timestamp = (edge[2] - pd.Timestamp('1970-01-01')).total_seconds()
-        return timestamp // period
+        return timestamp // period, edge[0], edge[1]
 
     def revert(tup):
         ts = pd.Timestamp(tup[0] * period, unit='s').isoformat()
         return (ts, tup[1])
 
     return grouper, revert
-
-
-def graph_binop(G: nx.Graph, edge):
-    G.add_edge(edge[0], edge[1])
-    return G
-
-
-def graph_combine(G1: nx.Graph, G2: nx.Graph):
-    g1 = set([e for e in G1.edges])
-    g2 = set([e for e in G2.edges])
-    G = nx.Graph()
-    G.add_edges_from(g1 | g2)
-    return G
 
 
 def get_components(t):
@@ -52,10 +39,25 @@ def get_components(t):
     G = t[1]
     partition = community.community_louvain.best_partition(G)
     communities = collections.defaultdict(set)
-    for node,part in partition.items():
+    for node, part in partition.items():
         communities[part].add(node)
     return t[0], list(communities.values())
 
+
+def combine_edges_to_graph(G: nx.Graph, edge):
+    G = G.copy()
+    G.add_edge(edge[0][1], edge[0][2], weight=edge[1])
+    return G
+
+
+def graph_combine(G1: nx.Graph, G2: nx.Graph):
+    g1 = {(e[0], e[1]): e[2] for e in G1.edges(data='weight')}
+    g2 = {(e[0], e[1]): e[2] for e in G2.edges(data='weight')}
+    all_edges = set(g1.keys()) | set(g2.keys())
+    G = nx.Graph()
+    for edge in all_edges:
+        G.add_edge(edge[0], edge[1], weight=g1.get(edge, 0) + g2.get(edge, 0))
+    return G
 
 
 def flatten(group):
@@ -65,8 +67,7 @@ def flatten(group):
     } for component in group[1]]
 
 
-def find_components(inpath="data/02_normalize/*.csv", outpath="data/03_find_components/*.ndjson", period=30 * 60):
-
+def find_components(inpath="data/02_normalize/*.csv", outpath="data/03_find_components/*.ndjson", period=24 * 60 * 60):
     with open("data/meta/period.txt", "w+") as f:
         f.write("<INVALID>")
 
@@ -75,12 +76,18 @@ def find_components(inpath="data/02_normalize/*.csv", outpath="data/03_find_comp
     edges = edges.map(parse_edge).filter(not_none)
 
     key, revert = make_grouper(period)
-    slices = edges.foldby(key, graph_binop, nx.Graph(), graph_combine, nx.Graph()).map(revert).repartition(12).persist()
+    sliced_weighted_edges = edges.foldby(key, lambda x, _: x + 1, 0, lambda x, y: x + y, 0).repartition(30)
+    slices = sliced_weighted_edges.foldby(lambda x: x[0][0], combine_edges_to_graph, nx.Graph(), graph_combine,
+                                          nx.Graph())
+    slices = slices.repartition(12).persist()
 
     # store the first graph for diagnostics
     example_ts, example_G = slices.take(1)[0]
+
+    weights = [e[2] for e in example_G.edges(data='weight')]
+    weights = [w/max(weights) for w in weights]
     nx.write_edgelist(example_G, "diagnostics/03_find_components/network_structure.txt")
-    nx.draw_networkx(example_G, with_labels=False, node_size=20)
+    nx.draw_circular(example_G, with_labels=False, node_size=20, edge_color=weights)
     plt.title(example_ts)
     plt.savefig("diagnostics/03_find_components/network_structure.png")
 
@@ -91,6 +98,7 @@ def find_components(inpath="data/02_normalize/*.csv", outpath="data/03_find_comp
 
     with open("data/meta/period.txt", "w+") as f:
         f.write(str(period))
+
 
 if __name__ == '__main__':
     cluster = LocalCluster(n_workers=12, threads_per_worker=1, memory_limit='166GB')
